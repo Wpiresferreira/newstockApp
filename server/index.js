c2addf07062b30398c9da9d0f05cbe8b2b91d066
Wagner Pires Ferreira <wagner_pires@icloud.com>
2024-12-21 14:03:04 -0700

    origin adjust

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());  // Parse cookies attached to client requests
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", origin);
+    // res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     if (req.method === "OPTIONS") {
